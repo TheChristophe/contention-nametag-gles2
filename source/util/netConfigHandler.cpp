173f275be7eb0efc8ea97ef63ede99cb8380f968
Christophe Laures <christophe.misc+gitlab@protonmail.ch>
2020-03-19 00:31:28 +0100

    Add support for referencing and deleting elements

--- source/util/netConfigHandler.cpp
+++ source/util/netConfigHandler.cpp
@@ -18,43 +18,88 @@ static struct mq_attr queueConfig {
 
 NetConfigHandler::NetConfigHandler(AnimationController *controller)
     : _controller(controller)
-    , _queue(mq_open("/nametag", O_RDONLY | O_CREAT | O_NONBLOCK, S_IRWXU, &queueConfig))
+    , _inQueue(mq_open("/nametag", O_RDONLY | O_CREAT | O_NONBLOCK, S_IRWXU, &queueConfig))
+    , _outQueue(mq_open("/nametag-out", O_WRONLY | O_CREAT | O_NONBLOCK, S_IRWXU, &queueConfig))
 {
-    if (_queue == static_cast<mqd_t>(-1)) {
+    if (_inQueue == static_cast<mqd_t>(-1)) {
         throw std::runtime_error(std::string("failed to open message queue: ") + strerror(errno));
     }
 }
 
 NetConfigHandler::~NetConfigHandler()
 {
-    mq_close(_queue);
+    mq_close(_inQueue);
     // kill the queue here because we are the receiving end
     mq_unlink("/nametag");
+    mq_close(_outQueue);
+    // kill the queue here because we are the receiving end
+    mq_unlink("/nametag-out");
 }
 
 void NetConfigHandler::Poll()
 {
-    ssize_t size{ mq_receive(_queue, _buffer, sizeof(_buffer) - 1, nullptr) };
+    ssize_t size{ mq_receive(_inQueue, _buffer, sizeof(_buffer) - 1, nullptr) };
     while (size != -1) {
         using json = nlohmann::json;
 
         _buffer[4096] = '\0';
         puts(_buffer);
 
-        auto data = json::parse(_buffer);
+        auto metadata = json::parse(_buffer);
 
-        if (data["type"] == "post") {
-            if (data["content"]["type"] == "triangle") {
-                auto x = std::stof(data["content"]["x"].get<std::string>());
-                auto y = std::stof(data["content"]["y"].get<std::string>());
-                _controller->AddTriangle(x, y);
-            }
-        }
-        else if (data["type"] == "put") {
-        }
-        else if (data["type"] == "get") {
-        }
+        Handle(metadata);
 
-        size = mq_receive(_queue, _buffer, sizeof(_buffer) - 1, nullptr);
+        size = mq_receive(_inQueue, _buffer, sizeof(_buffer) - 1, nullptr);
+    }
+}
+
+void NetConfigHandler::Handle(const nlohmann::json &metadata)
+{
+    using json = nlohmann::json;
+
+    if (metadata["type"] == "post") {
+        if (metadata["content"]["type"] == "triangle") {
+            auto &data = metadata["content"];
+            auto x{ std::stof(data["x"].get<std::string>()) };
+            auto y{ std::stof(data["y"].get<std::string>()) };
+            auto id{ _controller->AddTriangle(x, y) };
+
+            Respond(json{
+                { "uuid", metadata["uuid"] },
+                { "id", id } });
+        }
+    }
+    else if (metadata["type"] == "put") {
+        if (metadata["content"]["type"] == "_delete") {
+            auto &data = metadata["content"];
+            auto id{ data["id"].get<int>() };
+            _controller->Remove(id);
+        }
+    }
+    else if (metadata["type"] == "get") {
+    }
+}
+
+void NetConfigHandler::Respond(const nlohmann::json &response)
+{
+    auto str = response.dump();
+    if (mq_send(_outQueue, str.c_str(), str.length() + 1, 0) == -1) {
+        switch (errno) {
+            case EAGAIN: {
+                // trying to send message before webserver has finished reading them
+                std::fprintf(stderr, "Unexpected condition (%s:%i)", __FILE__, __LINE__);
+                std::exit(1);
+            } break;
+            case EINTR: {
+                // sending was interrupted by signal (how to respond to this?)
+                std::fprintf(stderr, "Unexpected condition (%s:%i)", __FILE__, __LINE__);
+                std::exit(1);
+            } break;
+            case EBADF: {
+                // invalid file descriptor, try recreating
+                mq_close(_outQueue);
+                _outQueue = mq_open("/nametag-out", O_WRONLY | O_CREAT | O_NONBLOCK, S_IRWXU, &queueConfig);
+            } break;
+        }
     }
 }
--- source/util/netConfigHandler.hpp
+++ source/util/netConfigHandler.hpp
@@ -1,6 +1,8 @@
 #ifndef NET_CONFIG_HANDLER_HPP
 #define NET_CONFIG_HANDLER_HPP
 
+#include <nlohmann/json_fwd.hpp>
+
 #include <mqueue.h> //mqd_t
 
 #include <cstdint>
@@ -13,8 +15,11 @@ class NetConfigHandler {
     void Poll();
 
     private:
+    void Handle(const nlohmann::json &metadata);
+    void Respond(const nlohmann::json &response);
     class AnimationController *_controller;
-    mqd_t _queue;
+    mqd_t _inQueue;
+    mqd_t _outQueue;
     char _buffer[4097];
 };
 
